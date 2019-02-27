be6adb9bfe306b6e9aea463d63c0060749e5adf5
Alan Layng <alan@readersludlow.com>
2019-02-27 22:27:27 +0000

    added service worker

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 'use strict'
 
 import '@babel/polyfill'
+import registerServiceWorker from './register-service-worker'
+import config from './config'
 import { createView } from './domvm'
 
 import { init, start } from './store'
 import App from './components/App'
 
 import './style/index.scss'
+
+registerServiceWorker(config.basePath + '/service-worker.js')
 
 init()
 start()
